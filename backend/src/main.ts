bdaecbc7da86a0cdd25a2716d59a1fece4d6385b
micha <michaeltkd14@gmail.com>
2026-05-04 19:50:07 -0600

    cambiando cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.enableCors();
+  app.enableCors(
+    {
+    origin: ['https://empowering-healing-production-a1c8.up.railway.app', 'http://localhost:4200'],
+    methods: ['GET', 'POST'],
+  }
+  );
 
   await app.listen(3000);
   console.log('Backend corriendo en http://localhost:3000');
