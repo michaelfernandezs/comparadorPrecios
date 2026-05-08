263056947566965485557778d482bfd5e3ecfb43
micha <michaeltkd14@gmail.com>
2026-05-08 15:22:44 -0600

    cambiando cores

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,11 +4,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.enableCors({
-    origin: ['https://comparador-precios-beta.vercel.app', 'http://localhost:4200'],
-    methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type'],
-  });
+ app.enableCors({
+  origin: (origin, callback) => {
+    if (!origin || origin.includes('vercel.app') || origin.includes('localhost')) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+});
 
   await app.listen(process.env.PORT || 3000);
 }
